10611d3d0f66b067bd23533d825247f74befb0cf
bllsglm <bilalsaglam92@gmail.com>
2024-04-24 16:00:18 +0300

    fix some cors bugs

--- frontend/src/slices/apiSlice.tsx
+++ frontend/src/slices/apiSlice.tsx
@@ -1,6 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export const BASE_URL = 'http://localhost:5000/api'
+export const BASE_URL =
+  import.meta.env.VITE_NODE_ENV === 'development'
+    ? 'http://localhost:5000/api'
+    : 'https://savecoordinates.onrender.com/'
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, credentials: 'include' })
 
