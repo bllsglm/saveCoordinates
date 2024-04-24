834f7a3720a045ad4f4999f6154982856b1d43c2
bllsglm <bilalsaglam92@gmail.com>
2024-04-24 16:10:03 +0300

    solve cors error

--- frontend/src/slices/apiSlice.tsx
+++ frontend/src/slices/apiSlice.tsx
@@ -1,9 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export const BASE_URL =
-  import.meta.env.VITE_NODE_ENV === 'development'
-    ? 'http://localhost:5000/api'
-    : 'https://savecoordinates.onrender.com/'
+export const BASE_URL = 'http://localhost:5000/api'
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, credentials: 'include' })
 
